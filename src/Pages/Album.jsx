dccb2f9486849b14dc6692c9fd884627331b0de0
Artilonhyseni <146942529+Artilonhyseni@users.noreply.github.com>
2024-10-22 22:48:50 +0200

    Album page i gang

--- src/Pages/Album.jsx
+++ src/Pages/Album.jsx
@@ -1,3 +1,45 @@
+import React from "react";
+
+// Import album cover and music icons
+import albumcover from "../images/to-pimp-a-butterfly.png";
+import soundcloud from "../images/Soundcloud.jpg";
+import apple from "../images/Apple.png";
+import spotify from "../images/Spotify.png";
+
 export default function Album() {
-  return;
+  return (
+    <div className="album-display-section">
+      <div className="album-header-info">
+        <img
+          src={albumcover}
+          alt="to-pimp-a-butterfly-album"
+          className="album-cover"
+        />
+        <div className="album-details-info">
+          <h1 className="album-title-heading">To Pimp A Butterfly</h1>
+          <p className="album-meta">Avg Rating: 3.45</p>
+          <div className="music-icons">
+            <img src={soundcloud} alt="soundcloud" className="icon" />
+            <img src={apple} alt="apple" className="icon" />
+            <img src={spotify} alt="spotify" className="icon" />
+          </div>
+        </div>
+      </div>
+
+      <div className="songs-section-album">
+        <h2 className="songs-title-header">Songs</h2>
+        <ul className="songs-list-items">
+          <li className="song-row-item">
+            <div className="song-info-text">
+              <p className="singer-name-text">Kendrick Lamar</p>
+            </div>
+            <div className="song-details-info">
+              <p>3:43</p>
+              <p>Rating: 3.5/10</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
