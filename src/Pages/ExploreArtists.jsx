5676dea2aec8a645de8eaa8f7d9cd607005bb274
WilliamBendtsen <eaa23wfb@students.eaaa.dk>
2024-10-22 11:55:13 +0200

    artists done

--- src/Pages/ExploreArtists.jsx
+++ src/Pages/ExploreArtists.jsx
@@ -1,18 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import kendrick from "../images/explore-artists/kendrick-lamar.png";
+import KendrickLamar from "../images/explore-artists/kendrick-lamar.png";
+import Drake from "../images/explore-artists/drake.png";
+import Sza from "../images/explore-artists/sza.png";
+import TaylorSwift from "../images/explore-artists/taylor-swift.png";
+import EdSheeran from "../images/explore-artists/ed-sheeran.png";
+import BillieEilish from "../images/explore-artists/billie-eilish.png";
+import TheWeeknd from "../images/explore-artists/the-weeknd.png";
+import PostMalone from "../images/explore-artists/post-malone.png";
+import ArianaGrande from "../images/explore-artists/ariana-grande.png";
 
 export default function ExploreArtists() {
   const artists = [
-    { name: "Kendrick Lamar", image: { kendrick } },
-    { name: "Drake", image: "path_to_drake_image" },
-    { name: "SZA", image: "path_to_sza_image" },
-    { name: "Taylor Swift", image: "path_to_taylor_image" },
-    { name: "Ed Sheeran", image: "path_to_ed_image" },
-    { name: "Billie Eilish", image: "path_to_billie_image" },
-    { name: "The Weeknd", image: "path_to_weeknd_image" },
-    { name: "Post Malone", image: "path_to_post_image" },
-    { name: "Ariana Grande", image: "path_to_ariana_image" },
+    { name: "Kendrick Lamar", image: KendrickLamar },
+    { name: "Drake", image: Drake },
+    { name: "SZA", image: Sza },
+    { name: "Taylor Swift", image: TaylorSwift },
+    { name: "Ed Sheeran", image: EdSheeran },
+    { name: "Billie Eilish", image: BillieEilish },
+    { name: "The Weeknd", image: TheWeeknd },
+    { name: "Post Malone", image: PostMalone },
+    { name: "Ariana Grande", image: ArianaGrande },
   ];
   return (
     <div className="artist-explore-container">
