6593a0f07b273cf390407884254bd11e4477832a
WilliamBendtsen <eaa23wfb@students.eaaa.dk>
2024-10-24 12:38:09 +0200

    reviews

--- src/Pages/Reviews.jsx
+++ src/Pages/Reviews.jsx
@@ -6,6 +6,13 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Reviews() {
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength) + "...";
+    }
+    return text;
+  };
+
   const reviews = [
     {
       reviewTitle: "Is this the peak of hip hop?",
@@ -16,27 +23,39 @@ export default function Reviews() {
       artist: "Kendrick Lamar",
       year: "2015",
       genres: "Conscious Hip Hop, West Coast Hip Hop, Jazz Rap",
+      review:
+        "This album can only be described as perfect. The mixing, instrumentation, and vocal performances are astounding. Every time I come back to this masterpiece I always pick up on something new. The approach that Kendrick takes on struggles of the BLM movement and living in the US as a person of color is well depicted and has changed the way I think about the issues. I could revisit every track on this album individually, the standouts for me are: The opener 'Wesley's Theory' George Clinton and Thundercat add greatly to the psychedelic funk feel of the opener with the ending flows greatly into 'For Free','King Kunta' has a more simple feeling but still dose a great job in its instrumentation and vocals, 'Alrigh' is my personal favorite single from the album I love how Kendrick raps over Pharells beat especially the bridge of the song with a great transition into the second interlude of the album. This album deserves all the praise it gets you can really tell Kendrick worked hard to perfect this album as every song is a 10/10.",
       likes: 204,
       image: TPAB,
       profileImage: placeholder,
     },
     {
-      reviewTitle: "Overskrift",
-      date: "21/4",
-      rating: 4,
+      reviewTitle: "Is this the peak of hip hop?",
+      date: "20/4",
+      rating: 5,
       profileName: "Pug Fartmeister",
-      album: "Eminem - The Eminem Show",
-      likes: 150,
+      album: "To Pimp A Butterfly",
+      artist: "Kendrick Lamar",
+      year: "2015",
+      genres: "Conscious Hip Hop, West Coast Hip Hop, Jazz Rap",
+      review:
+        "This album can only be described as perfect. The mixing, instrumentation, and vocal performances are astounding. Every time I come back to this masterpiece I always pick up on something new. The approach that Kendrick takes on struggles of the BLM movement and living in the US as a person of color is well depicted and has changed the way I think about the issues. I could revisit every track on this album individually, the standouts for me are: The opener 'Wesley's Theory' George Clinton and Thundercat add greatly to the psychedelic funk feel of the opener with the ending flows greatly into 'For Free','King Kunta' has a more simple feeling but still dose a great job in its instrumentation and vocals, 'Alrigh' is my personal favorite single from the album I love how Kendrick raps over Pharells beat especially the bridge of the song with a great transition into the second interlude of the album. This album deserves all the praise it gets you can really tell Kendrick worked hard to perfect this album as every song is a 10/10.",
+      likes: 204,
       image: Madvillainy,
       profileImage: placeholder,
     },
     {
-      reviewTitle: "Overskrift",
-      date: "22/4",
-      rating: 4,
+      reviewTitle: "Is this the peak of hip hop?",
+      date: "20/4",
+      rating: 5,
       profileName: "Pug Fartmeister",
-      album: "Eminem - The Eminem Show",
-      likes: 100,
+      album: "To Pimp A Butterfly",
+      artist: "Kendrick Lamar",
+      year: "2015",
+      genres: "Conscious Hip Hop, West Coast Hip Hop, Jazz Rap",
+      review:
+        "This album can only be described as perfect. The mixing, instrumentation, and vocal performances are astounding. Every time I come back to this masterpiece I always pick up on something new. The approach that Kendrick takes on struggles of the BLM movement and living in the US as a person of color is well depicted and has changed the way I think about the issues. I could revisit every track on this album individually, the standouts for me are: The opener 'Wesley's Theory' George Clinton and Thundercat add greatly to the psychedelic funk feel of the opener with the ending flows greatly into 'For Free','King Kunta' has a more simple feeling but still dose a great job in its instrumentation and vocals, 'Alrigh' is my personal favorite single from the album I love how Kendrick raps over Pharells beat especially the bridge of the song with a great transition into the second interlude of the album. This album deserves all the praise it gets you can really tell Kendrick worked hard to perfect this album as every song is a 10/10.",
+      likes: 204,
       image: Rumours,
       profileImage: placeholder,
     },
@@ -83,15 +102,10 @@ export default function Reviews() {
               <h2 className="review-album-title">{review.album}</h2>
               <p className="review-album-year">{review.year}</p>
               <p className="review-artist-name">{review.artist}</p>
-              <ul>
-                <li className="review-genres">{review.genres}</li>
-              </ul>
+              <p className="review-genres">{review.genres}</p>
             </div>
             <div className="main-review-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam
-              quasi similique molestias, enim dicta odit provident omnis veniam
-              consequatur maxime, optio incidunt nobis dolor consequuntur?
-              Laborum quis distinctio tempore dignissimos.
+              <p>{truncateText(review.review, 250)}</p>
             </div>
           </div>
           <p className="review-likes">
