59b0afd2ca0325601162e29db05411c74edd96c0
MortenKirch <morten-kris@live.dk>
2024-10-30 10:30:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/u-music-webapp/"}>
-      <App />
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/u-music-webapp/"}>
+        <App />
     </BrowserRouter>
   </StrictMode>
 );
